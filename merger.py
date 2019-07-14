f9ed2417da99bf2c3b5c205efda73717efd646c5
Yacine Haddad <yhaddad@cern.ch>
2019-07-14 15:47:49 +0200

    Update merger.py
    skip the merged directory

--- merger.py
+++ merger.py
@@ -5,6 +5,7 @@ outdir = "/eos/cms/store/group/phys_exotica/monoZ/LionKing2017/merged2017"
 
 for sample in os.listdir(indir):
     print sample
+    if "merged" in sample: continue
     command = "./haddnano.py {outdir}/{sample}.root {indir}/{sample}/*.root".format(
                 sample = sample,
                 outdir = outdir,
